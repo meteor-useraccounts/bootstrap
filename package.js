b7dfe0d6aad4d79a76f3e026458aa33fb9b4e7e7
Luca Mussi <ing.luca.mussi@gmail.com>
2014-04-24 01:38:39 +0200

    first stub

--- package.js
+++ package.js
@@ -1,13 +1,18 @@
 Package.describe({
-  summary: "Users' Accounts Systems templates styled for Twitter Bootstrap."
+    summary: "Users' Accounts Systems templates styled for Twitter Bootstrap."
 });
 
-Package.on_use(function (api, where) {
-  api.add_files('lib/accounts-users-bootstrap.js', ['client', 'server']);
+Package.on_use(function(api, where) {
+	api.imply('accounts-users-core', ['client', 'server']);
+    api.use(['templating'], 'client');
+    api.add_files([
+        'lib/accounts-users-bootstrap.html',
+        'lib/accounts-users-bootstrap.js'
+    ], ['client']);
 });
 
 Package.on_test(function(api) {
-  api.use('accounts-users-bootstrap');
-  api.use(['tinytest', 'test-helpers'], ['client', 'server']);
-  api.add_files('tests/accounts-users-bootstrap_tests.js', ['client', 'server']);
+    api.use('accounts-users-bootstrap');
+    api.use(['tinytest', 'test-helpers'], ['client', 'server']);
+    api.add_files('tests/accounts-users-bootstrap_tests.js', ['client', 'server']);
 });
