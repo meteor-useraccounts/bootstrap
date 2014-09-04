4dec65302b1d7af194627c8eb23b603a0dfb13d3
Luca Mussi <ing.luca.mussi@gmail.com>
2014-09-04 02:58:34 +0200

    Bump to version 0.0.24

--- package.js
+++ package.js
@@ -1,15 +1,51 @@
 Package.describe({
-    summary: "Accounts Templates styled for Twitter Bootstrap."
+    summary: "Accounts Templates styled for Twitter Bootstrap.",
+    version: "0.0.24",
+    name: "splendido:accounts-templates-bootstrap",
+    git: "https://github.com/splendido/accounts-templates-bootstrap.git",
 });
 
 Package.on_use(function(api, where) {
-    api.use([
-        'service-configuration',
-        'accounts-base',
-        'accounts-templates-core',
-        'templating',
-        'less'
-    ], 'client');
+    if (api.versionsFrom) {
+        api.use([
+            'service-configuration@1.0.0',
+            'accounts-base@1.0.0',
+            'splendido:accounts-templates-core@0.0.24',
+            'templating@1.0.4',
+            'less@1.0.5'
+        ], 'client');
+        api.use([
+            'service-configuration@1.0.0',
+            'accounts-password@1.0.0',
+            'accounts-base@1.0.0',
+            'splendido:accounts-templates-core@0.0.24',
+        ], 'server');
+
+        api.imply([
+            'splendido:accounts-templates-core@0.0.24',
+            'service-configuration',
+        ], ['client', 'server']);
+    }
+    else{
+        api.use([
+            'service-configuration',
+            'accounts-base',
+            'accounts-templates-core',
+            'templating',
+            'less'
+        ], 'client');        
+        api.use([
+            'service-configuration',
+            'accounts-password',
+            'accounts-base',
+            'accounts-templates-core',
+        ], 'server');
+
+        api.imply([
+            'accounts-templates-core',
+            'service-configuration',
+        ], ['client', 'server']);
+    }
 
     api.add_files([
         'lib/at_form.html',
@@ -21,25 +57,21 @@ Package.on_use(function(api, where) {
         'lib/full_page_at_form.html',
         'lib/at_bootstrap.less'
     ], ['client']);
-
-    api.use([
-        'service-configuration',
-        'accounts-password',
-        'accounts-base',
-        'accounts-templates-core',
-    ], 'server');
-
-    api.imply([
-        'accounts-templates-core',
-        'service-configuration',
-    ], ['client', 'server']);
 });
 
 Package.on_test(function(api) {
-    api.use([
-        'accounts-templates-bootstrap',
-        'accounts-templates-core',
-    ]);
+    if (api.versionsFrom) {
+        api.use([
+            'splendido:accounts-templates-bootstrap',
+            'splendido:accounts-templates-core@0.0.24',
+        ]);
+    }
+    else{
+        api.use([
+            'accounts-templates-bootstrap',
+            'accounts-templates-core',
+        ]);        
+    }
     api.use(['tinytest', 'test-helpers'], ['client', 'server']);
     api.add_files('tests/accounts-templates-bootstrap_tests.js', ['client', 'server']);
 });
