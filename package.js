25212b70b11d8e1aa432bdb52cfbad20f1aa8499
Luca Mussi <ing.luca.mussi@gmail.com>
2014-09-17 21:54:25 +0200

    Bump to version 0.9.0!

--- package.js
+++ package.js
@@ -7,16 +7,16 @@ Package.describe({
 
 Package.on_use(function(api, where) {
     api.use([
-        'accounts-templates-core',
+        'splendido:accounts-templates-core@0.9.0',
         'templating',
         'less'
     ], 'client');
     api.use([
-        'accounts-templates-core',
+        'splendido:accounts-templates-core@0.9.0',
     ], 'server');
 
     api.imply([
-        'accounts-templates-core',
+        'splendido:accounts-templates-core@0.9.0',
     ], ['client', 'server']);
 
     api.add_files([
@@ -55,8 +55,8 @@ Package.on_use(function(api, where) {
 
 Package.on_test(function(api) {
     api.use([
-        'accounts-templates-bootstrap',
-        'accounts-templates-core',
+        'splendido:accounts-templates-bootstrap',
+        'splendido:accounts-templates-core@0.9.0',
     ]);
     api.use(['tinytest', 'test-helpers'], ['client', 'server']);
     api.add_files('tests/accounts-templates-bootstrap_tests.js', ['client', 'server']);
