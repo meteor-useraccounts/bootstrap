0a267f68a067646f77960fa5f0d061aa74f113ca
Luca Mussi <ing.luca.mussi@gmail.com>
2015-05-02 09:13:15 +0200

    added missing files for new atMessage template

--- package.js
+++ package.js
@@ -28,6 +28,8 @@ Package.on_use(function(api, where) {
     'lib/at_form.js',
     'lib/at_input.html',
     'lib/at_input.js',
+    'lib/at_message.html',
+    'lib/at_message.js',
     'lib/at_nav_button.html',
     'lib/at_nav_button.js',
     'lib/at_oauth.html',
