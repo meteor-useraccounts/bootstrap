3dea007161d0b20d2848b4a7dfc01d0c3dc539a9
Luca Mussi <ing.luca.mussi@gmail.com>
2015-08-08 23:20:25 +0200

    minor release - Bump to version 1.12.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Accounts Templates styled for Twitter Bootstrap.',
-  version: '1.11.1',
+  version: '1.12.0',
   name: 'useraccounts:bootstrap',
   git: 'https://github.com/meteor-useraccounts/bootstrap.git',
 });
@@ -17,7 +17,7 @@ Package.on_use(function(api, where) {
   ], ['client', 'server']);
 
   api.imply([
-    'useraccounts:core@1.11.1',
+    'useraccounts:core@1.12.0',
   ], ['client', 'server']);
 
   api.add_files([
@@ -65,7 +65,7 @@ Package.on_use(function(api, where) {
 Package.on_test(function(api) {
   api.use([
     'useraccounts:bootstrap',
-    'useraccounts:core@1.11.1',
+    'useraccounts:core@1.12.0',
   ]);
 
   api.use([
