e1ddb2dc3df1367e0b8f2aa42f9a577700af8447
Luca Mussi <ing.luca.mussi@gmail.com>
2016-04-11 22:34:05 +0200

    patch release - Bump to version 1.14.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Accounts Templates styled for Twitter Bootstrap.',
-  version: '1.14.0',
+  version: '1.14.1',
   name: 'useraccounts:bootstrap',
   git: 'https://github.com/meteor-useraccounts/bootstrap.git',
 });
@@ -18,11 +18,11 @@ Package.on_use(function(api, where) {
   ], ['client', 'server']);
 
   // Requires all routing packages loads before this asking for weak dependencies.
-  api.use('useraccounts:flow-routing@1.14.0', ['client', 'server'], {weak: true});
-  api.use('useraccounts:iron-routing@1.14.0', ['client', 'server'], {weak: true});
+  api.use('useraccounts:flow-routing@1.14.1', ['client', 'server'], {weak: true});
+  api.use('useraccounts:iron-routing@1.14.1', ['client', 'server'], {weak: true});
 
   api.imply([
-    'useraccounts:core@1.14.0',
+    'useraccounts:core@1.14.1',
   ], ['client', 'server']);
 
   api.add_files([
@@ -70,7 +70,7 @@ Package.on_use(function(api, where) {
 Package.on_test(function(api) {
   api.use([
     'useraccounts:bootstrap',
-    'useraccounts:core@1.14.0',
+    'useraccounts:core@1.14.1',
   ]);
 
   api.use([
