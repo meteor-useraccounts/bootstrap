203b4a567ae2c6a0ad52c5df3fac5cc6f723193b
Luca Mussi <ing.luca.mussi@gmail.com>
2016-04-11 22:00:27 +0200

    minor release - Bump to version 1.14.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Accounts Templates styled for Twitter Bootstrap.',
-  version: '1.13.1',
+  version: '1.14.0',
   name: 'useraccounts:bootstrap',
   git: 'https://github.com/meteor-useraccounts/bootstrap.git',
 });
@@ -22,7 +22,7 @@ Package.on_use(function(api, where) {
   api.use('useraccounts:iron-routing@1.13.1', ['client', 'server'], {weak: true});
 
   api.imply([
-    'useraccounts:core@1.13.1',
+    'useraccounts:core@1.14.0',
   ], ['client', 'server']);
 
   api.add_files([
@@ -70,7 +70,7 @@ Package.on_use(function(api, where) {
 Package.on_test(function(api) {
   api.use([
     'useraccounts:bootstrap',
-    'useraccounts:core@1.13.1',
+    'useraccounts:core@1.14.0',
   ]);
 
   api.use([
